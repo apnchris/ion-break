929f52e2668f1668a30082f2cffa62ca01336932
apnchris <chris@apn.works>
2025-12-14 01:11:35 +0100

    Frontend content + Video frame

--- astro-app/src/utils/sanity.ts
+++ astro-app/src/utils/sanity.ts
@@ -18,6 +18,62 @@ export async function getPost(slug: string): Promise<Post> {
   );
 }
 
+export async function getTastes(): Promise<Taste[]> {
+  return await sanityClient.fetch(
+    groq`*[_type == "taste" && defined(slug.current)] | order(_createdAt desc)`
+  );
+}
+
+export async function getTaste(slug: string): Promise<Taste> {
+  return await sanityClient.fetch(
+    groq`*[_type == "taste" && slug.current == $slug][0]`,
+    {
+      slug,
+    }
+  );
+}
+
+export async function getBackgroundVideo(): Promise<BackgroundVideo | null> {
+  return await sanityClient.fetch(
+    groq`*[_type == "video"][0]{
+      "posterImage": posterImageShortClip.asset->url,
+      "videoFile": shortClipFile.asset->url,
+      "videoUrl": shortClipUrl
+    }`
+  );
+}
+
+export async function getFullVideo(): Promise<FullVideo | null> {
+  return await sanityClient.fetch(
+    groq`*[_type == "video"][0]{
+      "posterImage": posterImageFullVideo.asset->url,
+      "videoFile": fullVideoFile.asset->url,
+      "videoUrl": fullVideoUrl
+    }`
+  );
+}
+
+export interface BackgroundVideo {
+  posterImage?: string;
+  videoFile?: string;
+  videoUrl?: string;
+}
+
+export interface FullVideo {
+  posterImage?: string;
+  videoFile?: string;
+  videoUrl?: string;
+}
+
+export interface Taste {
+  _type: "taste";
+  _createdAt: string;
+  title?: string;
+  slug: Slug;
+  description?: string;
+  mainImage?: ImageAsset & { alt?: string };
+}
+
 export interface Post {
   _type: "post";
   _createdAt: string;
